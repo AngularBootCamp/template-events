15dd372acb0ccceced1b8fb8123fcb77c5b0c7c2
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-03-30 14:29:11 -0500

    curriculum update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,8 @@
-export const scheduleMap = {
+interface Schedule {
+  [key: string]: string;
+}
+
+export const scheduleMap: Schedule = {
   M: '9:00 - 5:00',
   T: '9:00 - 4:30',
   W: '9:00 - 3:00',
