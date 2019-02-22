2d094bb25c20f6b425e412efc8aba5319f6332dd
John Baur <john.baur@oasisdigital.com>
2019-02-22 11:05:56 -0600

    curriculum update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,4 @@
-interface Schedule {
-  [key: string]: string;
-}
-
-export const scheduleMap: Schedule = {
+export const scheduleMap: Record<string, string> = {
   M: '9:00 - 5:00',
   T: '9:00 - 4:30',
   W: '9:00 - 3:00',
