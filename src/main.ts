ad0336920e33793e7d87c82f77fbe99c5cd9514a
Zachary Kipping <zachary.kipping@oasisdigital.com>
2023-10-27 10:47:29 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // https://github.com/stackblitz/core/issues/2366
 import 'zone.js'; // Avoid error in StackBlitz
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'array-flat-polyfill'; // Avoid error in StackBlitz due to outdated TS supportimport { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
