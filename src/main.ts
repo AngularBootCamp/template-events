b24662028d0db78fd74b7ab3714dc1ebdf379528
Zachary Kipping <zachary.kipping@oasisdigital.com>
2024-07-29 11:02:52 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import 'zone.js'; // Avoid error in StackBlitz for Angular polyfill
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err =>
+  console.error(err)
+);
